7ee9a241a20f3a586e56a01aed45b1630b3bc805
Adonys Medina <adonysprogramming@gmail.com>
2020-09-06 17:36:21 -0400

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-4">
+      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+        <h2 className="text-2xl text-center">
+          ¿Cómo se calcula la base aerofotogrametrica de un par de fotos del
+          Google Earth?
+        </h2>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-4">
+        <button className="bg-white p-4 text-purple-800 font-bold rounded shadow mb-4">
+          Para conocer la altitud o altura ortométrica; es decir, respecto al
+          nivel del mar, se debe restar el valor de la anomalía a la altura
+          elipsoidal obteniendo en este caso una altitud de 160,3 metros.
+        </button>
+        <button className="bg-white p-4 text-purple-800 font-bold rounded shadow mb-4">
+          Respuesta 2
+        </button>
+        <button className="bg-white p-4 text-purple-800 font-bold rounded shadow">
+          Para conocer la altitud o altura ortométrica; es decir, respecto al
+          nivel del mar, se debe restar el valor de la anomalía a la altura
+          elipsoidal obteniendo en este caso una altitud de 160,3 metros.
+        </button>
+        <button className="bg-white p-4 text-purple-800 font-bold rounded shadow">
+          Respuesta 4
+        </button>
+      </div>
     </div>
   );
 }
