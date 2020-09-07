8f2fd2fbed9ad66b4bac17f56008073365646e50
Adonys <adonysmedinac@gmail.com>
2020-09-06 21:53:45 -0400

    Updates

---
+++ precache-manifest.95b1bda3f44a47b6143f8ad01f7d40ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9c65f5fc18e98abec2f29366f47060",
+    "url": "/quizzApp/tree/quizz/index.html"
+  },
+  {
+    "revision": "a34cbce2057b7a1dfdc7",
+    "url": "/quizzApp/tree/quizz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f495ecd88dccf54321e",
+    "url": "/quizzApp/tree/quizz/static/js/2.c1f46914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzApp/tree/quizz/static/js/2.c1f46914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34cbce2057b7a1dfdc7",
+    "url": "/quizzApp/tree/quizz/static/js/main.af25283a.chunk.js"
+  },
+  {
+    "revision": "b787d57218045c58abb7",
+    "url": "/quizzApp/tree/quizz/static/js/runtime-main.c32e18b4.js"
+  }
+]);
