be91da134f06a6f40d78119df3449b8096ce0933
Adonys <adonysmedinac@gmail.com>
2020-09-07 00:22:13 -0400

    agregar heroku api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Cuestionario } from "./components";
 
-const API_URL = "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple";
+const API_URL = "https://quizzgeoapi.herokuapp.com/preguntas";
 
 function App() {
   const [preguntas, setPreguntas] = useState([]);
