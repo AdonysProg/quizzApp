b420c83d934fac1d49e13c595da423517cc939f3
Adonys <adonysprogramming@gmail.com>
2020-10-27 15:06:40 -0400

    Agregar repasos de Hidro

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [minutes, setMinutes] = useState(0);
   const [isRunning, setIsRunning] = useState(true);
   useEffect(() => {
-    fetch(`./data.json`)
+    fetch(`./dataHidra.json`)
       .then((res) => res.json())
       .then((data) => {
         const preguntas = data.results
